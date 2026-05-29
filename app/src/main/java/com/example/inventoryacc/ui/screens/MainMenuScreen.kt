07bf1a660786ed6ad17671eefc2ad97e8bdd075f
DemakinDV <demakin.d.v@edu.mirea.ru>
2026-05-29 18:54:34 +0300

    MainMenuScreen small update

--- app/src/main/java/com/example/inventoryacc/ui/screens/MainMenuScreen.kt
+++ app/src/main/java/com/example/inventoryacc/ui/screens/MainMenuScreen.kt
@@ -3,12 +3,15 @@ package com.example.inventoryacc.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -20,11 +23,25 @@ fun MainMenuScreen(onLoginClick: () -> Unit) {
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "InventoryAcc",
-            modifier = Modifier.padding(bottom = 32.dp),
-            fontSize = 48.sp
+            text = "Добро пожаловать!",
+            modifier = Modifier.padding(bottom = 16.dp)
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally),
+            fontSize = 32.sp,
+            textAlign = TextAlign.Center
         )
-        Button(onClick = onLoginClick) {
+        Text(
+            text = "InventoryAcc — это удобное приложение для учета запасов, помогающее контролировать состояние товаров на складе вашего предприятия.",
+            modifier = Modifier.padding(bottom = 16.dp)
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center
+        )
+        Button(
+            onClick = onLoginClick,
+            modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally)
+        ) {
             Text("Начать работу")
         }
     }
